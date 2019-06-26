7e1b6f6cec0139accdfe32e910649c31001a53e7
bpandav <bpandav@DIN55001774.corp.capgemini.com>
2019-06-26 16:04:22 +0530

    NEW CLASS BY BHAVIN

---
+++ assignment 1/src/StartKare.java
@@ -0,0 +1,17 @@
+
+public class StartKare {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+
+int a=2,b=3,c;
+c=a+b;
+System.out.println("value will be::::::::"+c);
+	}
+
+
+
+	
+
+}
